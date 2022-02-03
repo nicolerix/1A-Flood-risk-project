de557c349da3b0c48d96fb5b01cedcaaaaadff77
Reece Linney <rl677@cam.ac.uk>
2022-02-03 21:07:19 +0000

    Code for distance list

---
+++ Task 1B.py
@@ -0,0 +1,15 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+def run():
+    stations = build_station_list()
+    p = (52.2053, 0.1218)
+
+    x = stations_by_distance(stations, p)
+    print(x[:10])
+
+if __name__ == "__main__":
+    print("* Task 1B: CUED Part 1A Flood Warning System *") 
+    run()  
+
+
