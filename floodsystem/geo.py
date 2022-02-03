0f5d31338892ae8f3e7e5eae1201fcee2a456095
Reece Linney <rl677@cam.ac.uk>
2022-02-03 21:08:17 +0000

    added for loop

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,20 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+from . import datafetcher
+from .station import MonitoringStation
+from floodsystem.stationdata import build_station_list
+
+
+distance = haversine(p, station.coord)
+
+def stations_by_distance(stations, p):
+    stations = build_station_list()
+    for station in stations:
+        names.append()
+        #coords = MonitoringStation.coord
+        distance.append()
+    turples = list(zip(names, distance))
+    turples = sorted_by_key(turples, 1)
+    return turples
