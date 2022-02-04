da51abfceee7982780a027965159ba6915d4cf45
Reece Linney <rl677@cam.ac.uk>
2022-02-04 13:33:37 +0000

    Sort list

--- Task 1C.py
+++ Task 1C.py
@@ -2,10 +2,11 @@ from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
 def run():
+    #Build list
     stations = build_station_list()
     centre = (52.2053, 0.1218)
     r = 10
-
+    # Sort list
     list = stations_within_radius(stations, centre, r)
     print(sorted(list))
 if __name__ == "__main__":
