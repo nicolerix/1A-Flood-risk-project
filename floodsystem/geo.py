8bb0caeac660a0204b566afeb545ea0f3ca6fa43
Nicole Rix <nr503@cam.ac.uk>
2022-02-05 12:20:39 +0000

    Task D E F

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,6 +6,7 @@ geographical data.
 
 """
 
+from os import name
 from .utils import sorted_by_key  # noqa
 from haversine import haversine
 from . import datafetcher
@@ -32,3 +33,24 @@ def stations_within_radius(stations, centre, r):
         else:
             pass
     return names
+
+
+def rivers_by_station_number(stations, N):
+    stations = build_station_list()
+    names= []
+
+# list of station objects that returns a set with names of the rivers with a monitoring station 
+def rivers_with_station(stations):
+    stationRivers= set()
+    for station in stations:
+        stationRivers.add(station.river)
+    stationRivers = sorted(stationRivers)
+    return stationRivers
+
+# using a dictionary mapping river names to a list of station objects on a given river  
+def stations_by_river(stations):
+    stationRivers = set(rivers_with_station(stations)) 
+    riversStations = {} #empty dictionary 
+    for stations in stations:
+        if not station.river in riversStations: 
+            riversStations.update({station.river: list})
